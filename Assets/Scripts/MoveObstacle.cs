17dbf81b8e6cc8fb5ace2cc0efb1911c476c4d88
Evgenij Kostadinov <ekostadinov5@gmail.com>
2020-05-13 12:31:45 +0200

    Circle filling fixed

--- Assets/Scripts/MoveObstacle.cs
+++ Assets/Scripts/MoveObstacle.cs
@@ -3,51 +3,38 @@ using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class MoveObstacle : MonoBehaviour {
-    private bool status;
-    private float gazeTime;
+    private bool clicked;
     private AudioSource buttonClickSound;
 
     public GameObject obstacle;
-	public Image timerImage;
-    public float requiredTime;
     public UnityEvent click;
 
     // Use this for initialization
     void Start()
     {
-        status = false;
-        gazeTime = 0F;
+        clicked = false;
         buttonClickSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (status)
-        {
-            gazeTime += Time.deltaTime;
-            timerImage.fillAmount = gazeTime / requiredTime;
-        }
-        if (gazeTime > requiredTime)
-        {
-            buttonClickSound.Play();
-            gazeTime = 0;
-            timerImage.fillAmount = 0;
-            GetComponent<Renderer>().material.color = Color.green;
-            click.Invoke();   
-        }
+
     }
 
     public void GazeOn()
     {
-        status = true;
+        if(!clicked)
+        {
+            clicked = true;
+            buttonClickSound.Play();
+            GetComponent<Renderer>().material.color = Color.green;
+            click.Invoke();
+        }
     }
 
     public void GazeOff()
     {
-        status = false;
-        gazeTime = 0F;
-        timerImage.fillAmount = 0;
     }
 
     public void Move()
